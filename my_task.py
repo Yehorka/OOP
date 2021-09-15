86525fc41a98ca3a542a4665cdf57e83287bd701
unknown <25kabachok25@gmail.com>
2021-09-15 23:04:12 +0300

    all done

--- my_task.py
+++ my_task.py
@@ -1,7 +1,7 @@
 import sys
-a = float(sys.argv[1])
-sign = sys.argv[2]
-b = float(sys.argv[3])
+a = float(sys.argv[1]) #first element of equation
+sign = sys.argv[2] #sign
+b = float(sys.argv[3]) #second element of equation
 if sign == "*":
     print(a * b)
 elif sign == "/":
