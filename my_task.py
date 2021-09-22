50576ae4ea3a2bf34e7fe05fba35aadf70e8db0e
unknown <25kabachok25@gmail.com>
2021-09-22 13:13:38 +0300

    done

--- my_task.py
+++ my_task.py
@@ -1,14 +1,7 @@
 import sys
-a = float(sys.argv[1]) #first element of equation
-sign = sys.argv[2] #sign
-b = float(sys.argv[3]) #second element of equation
-if sign == "*":
-    print(a * b)
-elif sign == "/":
-    print(a / b)
-elif sign == "+":
-    print(a + b)
-elif sign == "-":
-    print(a - b)
-else:
-    print("Error! Wrong arguments!")
+
+sys.argv.pop(0)
+try:
+    print(eval(' '.join(sys.argv)))
+except (NameError, SyntaxError):
+    print("Incorrect data")
--- my_task2.py
+++ my_task2.py
@@ -1,14 +1,8 @@
 import sys
-function = sys.argv[1]
-i = 2
-while i < len(sys.argv):
-    try:
-        function += sys.argv[i] #Filling string with equation
-        i += 1
-    except (IndexError, ValueError):
-        print("Error")
-        break
+import operator
+
 try:
-    print("True, " + str(eval(function))) #If equation runs, it is printed
-except NameError:
-    print("False") #If not - print false
+    function = getattr(operator, sys.argv[1])
+    print(function(float(sys.argv[2]), float(sys.argv[3])))
+except Exception:
+    print("Incorrect data")
