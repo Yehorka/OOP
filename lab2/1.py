0f6a1258c356aeefe1f6180643f7aab04c8ec25a
unknown <25kabachok25@gmail.com>
2021-09-30 15:51:53 +0300

    Laba 2 part 1 first attempt

--- lab2/1.py
+++ lab2/1.py
@@ -7,16 +7,12 @@ class Rectangle:
     def area(self):
         return (self.__width * self.__length)
     def set(self, width, length):
-        try:
-            if (type(width) != float or type(length) != float):
-                raise TypeError
-            if (0. < width < 20. ) and (0. < length < 20.):
-                self.__width = width
-                self.__length = length
-            else:
-                print("Width and length shoule be smaller than 20 and greater than 0")
-        except TypeError:
-            print("Width and length should be float value")
+        if not isinstance(width, float) or not (isinstance(length, float)):
+            raise TypeError
+        if not((0. < width < 20. ) and (0. < length < 20.)):
+            raise ValueError
+        self.__width = width
+        self.__length = length
     def get_length(self):
         return self.__length
     def get_width(self):
@@ -25,3 +21,4 @@ r = Rectangle()
 r.set(1., 19.0)
 print(r.get_width())
 print(r.area())
+print(r.perimeter())
--- lab2/2.py
+++ lab2/2.py
@@ -1,22 +1,19 @@
 import math
 class Rational:
-    def __init__(self, numerator=2, denominator=0):
-        try:
-            if (denominator == 0):
-                raise ZeroDivisionError("Division by Zero")
-            if not (isinstance(numerator,int)) or not (isinstance(denominator, int)):
-                raise TypeError("Value must be int")
-            self.__denominator = int(denominator / math.gcd(numerator,denominator))
-            self.__numerator = int(numerator / math.gcd(numerator,denominator))
-        except (TypeError,ZeroDivisionError):
-            print("Error")
-    def print_rat(self):
-        print(self.__numerator, "/",self.__denominator)
-    def print_fl(self):
-        print(self.__numerator/self.__denominator)
+    def __init__(self, numerator=2, denominator=4):
+        if (denominator == 0):
+            raise ZeroDivisionError("Division by Zero")
+        if not (isinstance(numerator,int)) or not (isinstance(denominator, int)):
+            raise TypeError("Value must be int")
+        self.__denominator = int(denominator / math.gcd(numerator,denominator))
+        self.__numerator = int(numerator / math.gcd(numerator,denominator))
+    def rat(self):
+        return(str(self.__numerator) + '/' + str(self.__denominator))
+    def fl(self):
+        return(self.__numerator/self.__denominator)
 try:
     r = Rational()
-    r.print_rat()
-    r.print_fl()
+    print(r.rat())
+    print(r.fl())
 except AttributeError:
     print("Wrong arguments")
