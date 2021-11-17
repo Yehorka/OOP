1488985ea73fc75cbb06afa3c6ec7d333d10145b
Yehorka <25kabachok25@gmail.com>
2021-11-17 22:23:17 +0200

    Done first try

--- lab3/2.py
+++ lab3/2.py
@@ -1,11 +1,11 @@
 from json import load
+from json import dump
 from datetime import date
-from enum import Enum
-#class Pizzas(Enum):
+from random import randint
 
-class Pizza:
+class Random_Pizza:
     def __init__(self):
-        day = date.today().weekday()
+        day = randint(0,6)
         with open("pizza-of-the-day.json", "r") as f:
             data = load(f)
             self.ingredients = data[list(data)[day]]['ingredients']
@@ -22,7 +22,7 @@ class Pizza:
     @day.setter
     def day(self, day):
         if not isinstance(day, str):
-            raise TypeError("Must be list")
+            raise TypeError("Must be str")
         self.__day = day
 
     @ingredients.setter
@@ -58,17 +58,32 @@ class Pizza:
         for i in range(len(t)):
             k += str(i) + '  -  ' + t[i] + ' \n'
         return k
+    def form_order(self):
+        with open("pizza-of-the-day.json", "r") as f:
+            data = load(f)
+            dumped = data[self.day]
+            dumped['ingredients'] = self.ingredients
+            dumped['date'] = str(date.today())
+        with open("order.json", 'w') as f:
+            dump(dumped, f, indent = 4)
 
+
+class Pizza_of_the_day(Random_Pizza):
+    def __init__(self):
+        super().__init__()
+        with open("pizza-of-the-day.json", "r") as f:
+            self.day = list(load(f))[date.today().weekday()]
 try:
-    p = Pizza()
+    p = Pizza_of_the_day()
 
     while True:
         print('Your pizza:\n' + str(p))
-        t = input('Want to add something? If no - enter 0\n')
+        t = input('Want to add something? If no - enter 0 to save your order\n')
         if t == '0':
             break
         else:
             print(p.show_ingredients())
             p.add_ingredients(input('What did you want?\n'))
+            p.form_order()
 except:
     print("error")
